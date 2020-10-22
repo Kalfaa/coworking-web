1f8c9fe31999a3f40e831903c8ca656aeaf59812
Kalfa <theo92320@hotmail.fr>
2020-10-22 12:22:40 +0200

    First commit app web

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,9 @@ import {OpenSpace, WorkEvent} from '../interface/login';
 })
 export class HomeComponent implements OnInit {
   openSpaces: OpenSpace[];
+  weekday = ['Lundi', ' Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+  month = ['Janvier', ' Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
   constructor(private openSpaceService: OpenSpaceService) { }
 
   ngOnInit(): void {
@@ -19,10 +22,22 @@ export class HomeComponent implements OnInit {
       .subscribe(
         data => {
           this.openSpaces = data;
+          this.openSpaces.forEach( (openSpace) => {
+            openSpace.events.sort((a, b) => {
+              // Turn your strings into dates, and then subtract them
+              // to get a value that is either negative, positive, or zero.
+              // @ts-ignore
+              return new Date(b.date) - new Date(a.date);
+            });
+          });
         },
         error => {
           console.log(error);
         });
   }
 
+  formatDateEvent(event: WorkEvent): string{
+    const date: Date = new Date(event.date);
+    return this.weekday[date.getDay()] + ' ' + date.getDate() + ' ' + this.month[date.getMonth()] + ' ' + date.getFullYear();
+  }
 }
