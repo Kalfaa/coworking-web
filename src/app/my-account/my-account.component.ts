484b931673b704b4f18534865752eda55b6102de
Kalfaa <theo92320@hotmail.fr>
2020-10-28 23:43:09 +0100

    Add event on homepage

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user-service.service';
-import {RoomAvailable, SubscriptionType, UserProfile} from '../interface/login';
+import {RoomAvailable, SubscriptionType, UserProfile, WorkEvent} from '../interface/login';
 import {AuthService} from '../auth.service';
 import {first} from 'rxjs/internal/operators';
 import {Router} from '@angular/router';
+import {OpenSpaceService} from '../open-space.service';
 
 @Component({
   selector: 'app-my-account',
@@ -13,13 +14,19 @@ import {Router} from '@angular/router';
 export class MyAccountComponent implements OnInit {
   user: UserProfile;
   subscribe = 'Aucun abonnement';
-  constructor(private userService: UserService, private authService: AuthService, private router: Router) { }
+  reslist = [];
+  weekday = ['Lundi', ' Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+  month = ['Janvier', ' Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+  creationDate: string;
+
+  constructor(private userService: UserService, private authService: AuthService, private router: Router, private openSpaceService: OpenSpaceService) { }
 
   ngOnInit(): void {
     this.userService.readById(this.authService.userValue.user.id).pipe(first())
       .subscribe(
         data => {
             this.user = data;
+            this.creationDate = this.formatDateEvent(this.user.created);
             console.log(this.user.subscription.type);
             if (this.user.subscription && this.user.subscription.type === SubscriptionType.RESIDENT){
               this.subscribe = 'Abonnement résident jusqu\'au : ' + this.user.subscription.end  ;
@@ -31,10 +38,26 @@ export class MyAccountComponent implements OnInit {
         error => {
           console.log(error);
         });
+    this.openSpaceService.readReservation()
+      .pipe(first())
+      .subscribe(
+        data => {
+          // this.reservationForDate = data.reservations;
+          this.reslist = data;
+          // console.log(this.dataSource[0].room.openSpace.name);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
   goToSub(){
     this.router.navigate(['/add-subscription']);
+  }
+
+  formatDateEvent(dateString: any){
+    const date: Date = new Date(dateString);
+    return this.weekday[date.getDay()] + ' ' + date.getDate() + ' ' + this.month[date.getMonth()] + ' ' + date.getFullYear();
   }
 
 
